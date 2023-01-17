c04a321e84d76c349bcdf877f1f1ba363cdc223a
gabriel <gos.gabri@gmail.com>
2023-01-16 23:34:28 -0400

    Levantando a primeira rota com fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import Fastify from "fastify";
+
+const app = Fastify()
+
+// Metodo HTTP: Get, Post, Put, Patch, Delete
+
+app.get("/" , () => {
+    return "Hello World"
+})
+
+app.listen({
+    port: 3333,
+})
